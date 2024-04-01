336d72e09788f07bb14e01b42050a71bf3095ee8
LiZa224284 <yuxuanli224284@gmail.com>
2024-04-01 14:44:14 -0500

    Add dog funcionality

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def dog():
+	print(' Woof!')
+
 def defult():
 	print('hello')
 
 def main():
-	defult()
+	if sys.argv[1] == 'dog':
+		dog()
+	else:
+		defult()
 
 if __name__ == '__main__':
 	main()
